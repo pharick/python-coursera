3e24ac9385eada126e7c4753f71cd38181987fbf
Artem Forkunov <artemforkunov@gmail.com>
2019-03-20 10:10:22 +0300

    better pinguins

--- week1/2-pinguins.py
+++ week1/2-pinguins.py
@@ -1,6 +1,20 @@
+def pinguin(number):
+  lines = number // 10
+  last_line = number % 10
+
+  for _ in range(lines):
+    print("   _~_    " * 10)
+    print("  (o o)   " * 10)
+    print(" /  V  \\  " * 10)
+    print("/(  _  )\\ " * 10)
+    print("  ^^ ^^   " * 10)
+
+  if last_line > 0:
+    print("   _~_    " * last_line)
+    print("  (o o)   " * last_line)
+    print(" /  V  \\  " * last_line)
+    print("/(  _  )\\ " * last_line)
+    print("  ^^ ^^   " * last_line)
+
 number = int(input())
-print("   _~_    " * number)
-print("  (o o)   " * number)
-print(" /  V  \\  " * number)
-print("/(  _  )\\ " * number)
-print("  ^^ ^^   " * number)
+pinguin(number)
